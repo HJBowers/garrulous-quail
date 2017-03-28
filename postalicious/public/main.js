962082fe3b3238434a8dbaaba56390324d42d927
HJBowers <HJBowers@gmail.com>
2017-03-28 15:09:54 -0700

    updated html file and main.js file

---
+++ postalicious/public/main.js
@@ -0,0 +1,17 @@
+function buildRequest() {
+  var element = document.createElement('')
+  element.id = ''
+  var  = document.getElementsByClassName('queryParameters')
+
+  const method =
+  const host =
+
+//query
+
+
+//header
+
+
+//body
+
+}
