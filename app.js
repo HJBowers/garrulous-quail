3cab7a5e1a9110082354f9b7d8df43c47eba0751
HJBowers <HJBowers@gmail.com>
2017-03-28 09:35:55 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express'),
-      // bodyParser = require('body-parser'),
-      app = express()
-
-// app.use(bodyParser.json())
+      bodyParser = require('body-parser'),
+      app = express(),
+      jsonParser = bodyParser.json()
 
 app.get('/', function(req, res) {
   res.status(200)
@@ -11,40 +10,34 @@ app.get('/', function(req, res) {
 })
 
 app.get('/search', function(req, res) {
-  res.status(200)
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('You searched for:' + req.query.q)
-})
-
-app.get('/search', function(req,res) {
-  res.status(400)
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('You did not provide a search query term :(')
+  if(req.query.q) {
+    res.status(200)
+    res.setHeader('Content-Type', 'text/plain')
+    res.send('You searched for: ' + req.query.q)
+  }else{
+    res.status(400)
+    res.setHeader('Content-Type', 'text/plain')
+    res.send('You did not provide a search query term :(')
+  }
 })
 
 // Test in Postman
 app.post('/things', function(req, res) {
   res.status(201)
   res.setHeader('Content-Type', 'text/plain')
-  // res.send('New thing created:' +  req.body)
+  res.send('New thing created:' + req.body)
 })
 
 app.get('/somefile', function(req, res) {
-  res.status(200)
-  res.set({
-    'Content-Type': 'text/plain',
-    'Accept': 'text/plain'
-  })
-  res.send('This is a plain text file')
-})
-
-app.get('/somefile', function(req, res) {
-  res.status(200)
-  res.set({
-    'Content-Type': 'text/html',
-    'Accept': 'text/html'
-  })
-  res.send('<!DOCTYPE html><html><body>This is an HTML file</body></html>')
+  if(req.get('Accept', 'text/plain')) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.status(200)
+    res.send('This is a plain text file')
+  }else if(req.get('Accept', 'text/html')) {
+    res.setHeader('Content-Type', 'text/html')
+    res.status(200)
+    res.send('<!DOCTYPE html><html><body>This is an HTML file</body></html>')
+  }
 })
 
 app.get('/myjsondata', function(req, res) {
@@ -63,10 +56,10 @@ app.get('/old-page', function(req, res) {
 })
 
 // Test in Postman
-app.post('/admin-only', function(req, res) {
-  res.status(403)
-  res.send()
-})
+// app.post('/admin-only', function(req, res) {
+//   res.status(403)
+//   res.send()
+// })
 
 app.get('/not-a-page', function(req, res) {
   res.status(404)
