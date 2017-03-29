77d78a5a11c2d899eb55aec2e88efc001ae97fb8
HJBowers <HJBowers@gmail.com>
2017-03-29 11:49:50 -0700

    working on main.js... little progress

--- postalicious/public/main.js
+++ postalicious/public/main.js
@@ -1,9 +1,41 @@
-function buildRequest() {
-  var element = document.createElement('')
-  element.class = ''
+document.querySelectorAll( )
 
-  const method = document.getElementsByClassName('methodBox')
-  const host = document.getElementsByClassName('hostBox')
+function buildRequest() {
+  let superAwesomeHTTPInformationUltra = document.querySelectorAll('.form *')
+  console.log("superAwesomeHTTPInformationUltra", superAwesomeHTTPInformationUltra);
+  let valueArr = []
+  superAwesomeHTTPInformationUltra.forEach( element => {
+    if(element.tagName === INPUT || element.tagName === TEXTAREA) {
+      valueArr.push({
+        element.name,
+        element.value
+       })
+    }
+  })
+}
+
+
+
+// HTTP object = {
+//   method : Value,
+//   RequestURL : value,
+//   queryparameters : [
+//                       [key1, key2, key3],
+//                       [value1, value2, value3]
+//                     ],
+//   queryparameters : {
+//                     key1 : value1,
+//                     key2 : value2,
+//                     key3 : value3
+//                   },
+//   Headers : {
+//                     key1 : value1,
+//                     key2 : value2,
+//                     key3 : value3
+//                   },
+//   body: someString
+// }
+
 
 // //queryParameters
 //   const queryKeys = document.getElementsByClassName('qKeyBox')
@@ -12,5 +44,21 @@ function buildRequest() {
 // //queryParameters
 //   const queryKeys = document.getElementsByClassName('qKeyBox')
 //   const queryValues = document.getElementsByClassName('qValueBox')
+//
+// }
 
-}
+
+//step 1
+  // create a json object with form informatiion
+//step 2
+  //log the json object to the console
+//step 3
+  //display json object in the request box
+//step 4
+  //send json object to the sandbox server in the body of an http request
+//step5
+  //use a http library to make an http from the body of the http request
+//step 6
+  //get the response and send that back to the pl server
+// step 7
+  //display http body as an http request in the response box
