292c7e79ae81fad9dc005f48b44285c959f36ea8
John <jhallman5@gmail.com>
2017-03-29 16:38:04 -0700

    trying out http

--- postalicious/app.js
+++ postalicious/app.js
@@ -1,12 +1,13 @@
 const express = require('express'),
       app = express()
 
+app.use(express.static(__dirname + '/public'))
 
-
-
-
-
-
+app.get('/', function(req, res) {
+  res.status(200)
+  res.setHeader('Content-Type', 'text/plain')
+  res.render('index')
+})
 
 app.listen(3001, function() {
   console.log('Example app listening on port 3001!')
--- postalicious/public/main.js
+++ postalicious/public/main.js
@@ -1,17 +1,71 @@
+
 function buildRequest() {
-  var element = document.createElement('')
-  element.id = ''
-  var  = document.getElementsByClassName('queryParameters')
+  let superAwesomeHTTPInformationUltra = document.querySelectorAll('.form *')
+  console.log("superAwesomeHTTPInformationUltra", superAwesomeHTTPInformationUltra);
+  let valueArr = []
+  superAwesomeHTTPInformationUltra.forEach( element => {
+    if(element.tagName === "INPUT" || element.tagName === "textarea"){
+      if(element.tagName)
+      valueArr.push(
+        element.className,
+        element.value
+       )
+     }
+  })
+  console.log('-=-=- ',valueArr)
+}
 
-  const method =
-  const host =
 
-//query
+function buildRequest() {
+  let superAwesomeHTTPInformationUltra = document.querySelectorAll('.form *')
+  console.log("superAwesomeHTTPInformationUltra", superAwesomeHTTPInformationUltra);
+  let httpObject = {
+                queryParams :[],
+                headers : []
+              }
+  superAwesomeHTTPInformationUltra.forEach( element => {
+    if(element.className === "methodBox"){
+      httpObject.method = element.value
+    }
+    if(element.className === "hostBox"){
+      httpObject.host = element.value
+    }
+    if(element.className === "qkeyBox" || element.className ==='qvalueBox'){
+      httpObject.queryParams.push(element.value)
+    }
+    if(element.className === "hkeyBox" || element.className ==='hvalueBox'){
+      httpObject.headers.push(element.value)
+    }
+    if(element.tagName === "textarea"){
+      httpObject.body = element.value
+    }
+  })
+  console.log('-=-=- ',httpObject)
+  console.log('queryParams ', httpObject.queryParams)
+  console.log('headers ', httpObject.headers)
+}
+
+
+
+//step 1
+  // create a json object with form informatiion
+//step 2
+  //log the json object to the console
+//step 3
+  //display json object in the request box
+//step 4
+  //send json object to the sandbox server in the body of an http request
+//step5
+  //use a http library to make an http from the body of the http request
+//step 6
+  //get the response and send that back to the pl server
+// step 7
+  //display http body as an http request in the response box
 
 
 //header
 
 
 //body
-
-}
+//
+// }
