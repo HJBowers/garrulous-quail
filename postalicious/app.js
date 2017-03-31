dca7817e01535d2f42d41f4b25b9507ff7c23bf6
John <jhallman5@gmail.com>
2017-03-30 17:11:44 -0700

    working http request and response displays

--- postalicious/app.js
+++ postalicious/app.js
@@ -1,12 +1,49 @@
-const express = require('express'),
-      app = express()
+const express = require('express')
+const request = require('request')
+const bodyParser = require('body-parser')
+const app = express()
 
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   res.status(200)
   res.setHeader('Content-Type', 'text/plain')
   res.render('index')
+})
+
+app.post('/dorequest', function(req, res) {
+  console.log('WTF???', req.body)
+  const requestDetails = req.body
+  request({
+    method: requestDetails.method,
+    uri: requestDetails.host,
+  }, function(error, response){
+    console.log('????', error)
+    if (error){
+      res.json({
+        requestFailed: true,
+        error: error,
+      })
+    }else{
+      res.json(response)
+    }
+  })
+
+
+  // let info = JSON.parse(request.body)
+  // megaHTTPRequestULTRATON(info, (error, urlResponse, body) => {
+  //   if(error) {
+  //     console.log("NOOOOOO");
+  //   } else {
+  //     res.json({
+  //       'headers': urlResponse.headers,
+  //       'body': body,
+  //       'status': urlResponse.status,
+  //       'httpVersion': urlResponse.httpVersion
+  //     })
+  //   }
+  // })
 })
 
 app.listen(3001, function() {
--- postalicious/public/main.js
+++ postalicious/public/main.js
@@ -1,101 +1,151 @@
-//
-// function buildRequest() {
-//   let superAwesomeHTTPInformationUltra = document.querySelectorAll('.form *')
-//   console.log("superAwesomeHTTPInformationUltra", superAwesomeHTTPInformationUltra);
-//   let valueArr = []
-//   superAwesomeHTTPInformationUltra.forEach( element => {
-//     if(element.tagName === "INPUT" || element.tagName === "textarea"){
-//       if(element.tagName)
-//       valueArr.push(
-//         element.className,
-//         element.value
-//        )
-//      }
-//   })
-//   console.log('-=-=- ',valueArr)
-// }
-//
-//
-// function buildRequest() {
-//   let superAwesomeHTTPInformationUltra = document.querySelectorAll('.form *')
-//   console.log("superAwesomeHTTPInformationUltra", superAwesomeHTTPInformationUltra);
-//   let httpObject = {
-//                 queryParams :[],
-//                 headers : []
-//               }
-//   superAwesomeHTTPInformationUltra.forEach( element => {
-//     if(element.className === "methodBox"){
-//       httpObject.method = element.value
-//     }
-//     if(element.className === "hostBox"){
-//       httpObject.host = element.value
-//     }
-//     if(element.className === "qkeyBox" || element.className ==='qvalueBox'){
-//       httpObject.queryParams.push(element.value)
-//     }
-//     if(element.className === "hkeyBox" || element.className ==='hvalueBox'){
-//       httpObject.headers.push(element.value)
-//     }
-//     if(element.className === "bodyBox"){
-//       httpObject.body = element.value
-//     }
-//   })
-//   console.log('-=-=- ',httpObject)
-//   console.log('queryParams ', httpObject.queryParams)
-//   console.log('headers ', httpObject.headers)
-// }
-
 function valueFor(name) {
-  document.querySelector(`[name="${name}"]`).value
+  const node = document.querySelector(`[name="${name}"]`)
+  return node ? node.value : undefined
 }
 
-requestDetails = {
-  method: valueFor("method"),
-  host: valueFor("host"),
-  body: valueFor("body"),
-  headers: [
-    [
-      valueFor("headerKey1"),
-      valueFor("headerValue1")
+function getRequestDetails () {
+   return {
+    method: valueFor("method"),
+    host: valueFor("host"),
+    body: valueFor("body"),
+    headers: [
+      [
+        valueFor("headerKey1"),
+        valueFor("headerValue1")
+      ],
+      [
+        valueFor("headerKey2"),
+        valueFor("headerValue2")
+      ],
+      [
+        valueFor('headerKey3'),
+        valueFor('headerValue3')
+      ]
     ],
-    [
-      valueFor("headerKey2"),
-      valueFor("headerValue2")
-    ],
-    [
-      valueFor('headerKey3'),
-      valueFor('headerValue3')
+    queryParameters: [
+      [
+        valueFor("queryKey4"),
+        valueFor("queryValue4")
+      ],
+      [
+        valueFor("queryKey5"),
+        valueFor("queryValue5")
+      ],
+      [
+        valueFor("queryKey6"),
+        valueFor("queryValue6")
+      ]
     ]
-  ],
-  queryParameters: [
-    [
-      valueFor("queryKey4"),
-      valueFor("queryValue4")
-    ],
-    [
-      valueFor("queryKey5"),
-      valueFor("queryValue5")
-    ],
-    [
-      valueFor("queryKey6"),
-      valueFor("queryValue6")
-    ]
-  ]
+  }
 }
-console.log(requestDetails)
 
-// const requestDetailsToString = (requestDetails) => {
-//   `
-//   GET /foo/bar?a=1&b=2 HTTP/1.1
-//   Host: example.com
-//   User-Agent: curl/7.51.0
-//   Accept: */*
-//   `
-//
-//   let string =  ''
-//   string += requestDetails.method.toUpperCase() + ' '
-//
-// }
+// MAIN -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- MAIN
+function formSubmission(event) {
+  const requestDetails = getRequestDetails()
+  console.log('requestDetails ', requestDetails)
+  console.log('headers ', requestDetails.headers[0])
+  console.log('query ', requestDetails.queryParameters)
+  appendRequest(requestDetails)
+  doRequest(requestDetails)
+}
+
+
+function appendRequest(requestDetails) {
+  let node = document.createElement('pre')
+  node.innerText = requestDetailsToString(requestDetails)
+  document.querySelector('.HTTPRequestBody').appendChild(node)
+}
+
+function doRequest(requestDetails){
+  console.info('doing request', requestDetails)
+  console.info('doing request', JSON.stringify(requestDetails))
+  // send the http request details to the server
+  // via a transport http request
+  fetch('/dorequest', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestDetails),
+  })
+  // .then(response => response.body() )
+  .then(transportResponse => {
+    if (transportResponse.ok) return transportResponse.json()
+    console.error('failed to do request', response)
+  })
+  .then(requestResponse => {
+    if (requestResponse.requestFailed){
+      console.log(requestResponse)
+      document.querySelector('.HTTPResponseBody').innerText = ''+
+        'TOTAL FAILURE!!!!\n'+
+        JSON.stringify(requestResponse, null, 2)
+    }else{
+      console.log('resonse body', requestResponse)
+
+      document.querySelector('.HTTPResponseBody').innerText =
+          JSON.stringify(requestResponse, null, 2)
+    }
+  })
+}
+
+// REQUEST SHOULD LOOK LIKE EXAMPLE BELOW:
+// `
+// GET /foo/bar?a=1&b=2 HTTP/1.1
+// Host: example.com
+// User-Agent: curl/7.51.0
+// Accept: */*
+// `
+const requestDetailsToString = (requestDetails) => {
+  let startOfPath = requestDetails.host.indexOf('3001/') + 4
+  let path = requestDetails.host.slice(startOfPath)
+  let string =  ''
+  string += requestDetails.method.toUpperCase() + ' '
+  string += path + ' '
+  string += 'HTTP/1.1 ' + '\n'
+  string += 'HOST: ' + requestDetails.host.slice(0, startOfPath) + '\n'
+  string += 'User-Agent: ' + 'Postalicious' + '\n'
+  string += 'Accept: */* '
+  return string
+}
+
+function responseMessage() {
+  const requestDetails = getRequestDetails()
+  fetch('localhost:3001/responseMessage', {
+    method: 'POST',
+    headers: {
+      'Accept': 'text/plain'
+    },
+    body: JSON.stringify(requestDetails)
+  })
+  .then(response => response.json())
+  //RENDER RESPONSE HERE with helper function
+}
+
+// REPSONSE SHOULD LOOK LIKE EXAMPLE BELOW:
+/*
+HTTP/1.1 200 OK
+Date: Sun, 18 Oct 2009 08:56:53 GMT
+Server: Apache/2.2.14 (Win32)
+Last-Modified: Sat, 20 Nov 2004 07:16:26 GMT
+ETag: "10000000565a5-2c-3e94b66c2e680"
+Accept-Ranges: bytes
+Content-Length: 44
+Connection: close
+Content-Type: text/html
+X-Pad: avoid browser bug
+
+<html><body><h1>It works!</h1></body></html>
+*/
+function renderResponse(domValues) {
+  let responseDiv = document.querySelector('.HTTPResponseBody')
+  if('error' in domValues) {
+    console.log("renderResponse ERROR");
+  } else {
+    let
+  }
+}
+
 
 //step 1
   // create a json object with form informatiion
@@ -119,3 +169,6 @@ console.log(requestDetails)
 //body
 //
 // }
+
+
+document.querySelector('.requestButton').addEventListener('click', formSubmission)
